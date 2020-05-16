363da77056fb79b78b4228e96dbf214a7661a800
Reinoptland <reinoptland@gmail.com>
2020-05-16 16:41:49 +0200

    UPDATE_PROFILE only updates when the profile loaded

--- userReducer.js
+++ userReducer.js
@@ -39,6 +39,12 @@ module.exports = function reducer(state = initialState, action = {}) {
     }
 
     case "UPDATE_PROFILE": {
+      // if we have the more powerfull UPDATE_PROFILE action we might not need
+      // UPDATE_PROFILE_WEBSITE, UPDATE_PROFILE_INTRO, UPDATE_PROFILE_EMAIL anymore ...
+      // Unless they contain specific logic on how to update the state
+      if (state.me === null) {
+        return state;
+      }
       // easy solution with the spread operator:
       // simply spread the action payload
       return { ...state, me: { ...state.me, ...action.payload } };
